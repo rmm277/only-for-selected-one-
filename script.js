7044ac96a6b8fa77ef97ecd49d2fd03f8293d995
rmm277 <rmm27996@gmail.com>
2026-06-28 03:14:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.getElementById("startBtn").onclick = function() {
 
-    document.getElementById("main").style.opacity = "0";
 
-    setTimeout(function(){
+document.getElementById("main").style.opacity = "0";
 
-        document.getElementById("main").style.display = "none";
 
-        document.getElementById("gallery").style.display = "block";
+setTimeout(function(){
 
-    },1000);
+
+document.getElementById("main").style.display = "none";
+
+
+document.getElementById("gallery").style.display = "block";
+
+
+},1000);
+
 
 };
